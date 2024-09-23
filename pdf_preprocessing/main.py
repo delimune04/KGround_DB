42e99131b73530515a6d9bb854fdbfc2c25fd995
Sanghyeon Park <sp040122@gmail.com>
2024-09-24 02:58:47 +0900

    updated main.py for building .exe

--- pdf_preprocessing/main.py
+++ pdf_preprocessing/main.py
@@ -2,15 +2,16 @@ import os
 import cv2
 import numpy as np
 from pdf2image import convert_from_path
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, freeze_support
 from PIL import Image
 from detect_table import detect_tables_in_image
 from extract_table import extract_tables_from_pdf
 
-if __name__ == '__main__':
+def main():
     input_directory = input("PDF 결과물 폴더 경로를 복사 후 입력하세요: ")
     output_directory = input("결과를 저장할 폴더 경로를 복사 후 입력하세요: ")
 
+    # Normalize the paths to handle backslashes
     input_directory = os.path.normpath(input_directory)
     output_directory = os.path.normpath(output_directory)
 
@@ -21,11 +22,14 @@ if __name__ == '__main__':
         f for f in os.listdir(input_directory) if f.lower().endswith(".pdf")
     ]
 
+    # Inform the user about the available CPUs
     available_cpus = cpu_count()
     print(f"현재 컴퓨터의 CPU 개수는 {available_cpus}개 입니다.")
 
+    # Prompt the user to enter the number of CPUs to use
     num_processes = input("결과 처리에 사용할 CPU 수를 입력하세요 (전체 CPU의 절반 이하 권장): ")
 
+    # Validate and convert the input to an integer
     try:
         num_processes = int(num_processes)
         if num_processes < 1:
@@ -41,3 +45,7 @@ if __name__ == '__main__':
     with Pool(processes=num_processes) as pool:
         args = [(pdf_file, input_directory, output_directory) for pdf_file in pdf_files]
         pool.map(extract_tables_from_pdf, args)
+
+if __name__ == '__main__':
+    freeze_support()
+    main()
